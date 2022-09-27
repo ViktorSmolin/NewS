88e9974c274dbcaa606f4e6ba195b080abae88a0
Viktor Smolin <vsmolin8@gmail.com>
2022-09-27 22:54:19 +0700

    НИчего интересного

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Appearance from './components/appearance/Appearance';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     < div className="App">
         <Appearance/>
+        
     </div>
   );
 }
